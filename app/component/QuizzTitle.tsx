76c00551088c6c829a8ff267186a3032d3770c41
anuprajvarma <anupraj1854@gmail.com>
2025-07-05 05:16:19 +0530

    make quizzes topic page for all category

---
+++ app/component/QuizzTitle.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const QuizzTitle = ({ title }: { title: string }) => {
+  return (
+    <Link
+      href="#"
+      className="flex flex-col min-w-[15rem] px-6 py-4 border rounded-lg shadow-sm bg-gray-800 border-gray-700 hover:bg-gray-700"
+    >
+      <h5 className="text-2xl font-medium tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="font-normal text-end text-gray-700 dark:text-gray-400">
+        10 Qs
+      </p>
+    </Link>
+  );
+};
+
+export default QuizzTitle;
--- app/quizzes/[category]/page.tsx
+++ app/quizzes/[category]/page.tsx
@@ -1,11 +1,67 @@
 import React from "react";
+import QuizzTitle from "@/app/component/QuizzTitle";
+
+const allQuizzes = [
+  {
+    category: "Math",
+    quizzes: [
+      { id: "1", title: "Basic Algebra" },
+      { id: "2", title: "Geometry Fun" },
+    ],
+  },
+  {
+    category: "Science",
+    quizzes: [
+      { id: "3", title: "Physics Basics" },
+      { id: "4", title: "Chemistry 101" },
+    ],
+  },
+  {
+    category: "History",
+    quizzes: [
+      { id: "5", title: "World War II" },
+      { id: "6", title: "Ancient Civilizations" },
+    ],
+  },
+  {
+    category: "Programming",
+    quizzes: [
+      { id: "7", title: "JavaScript Essentials" },
+      { id: "8", title: "React Fundamentals" },
+    ],
+  },
+];
 
 interface Props {
   params: { category: string };
 }
 
 const Page = ({ params }: Props) => {
-  return <div className="text-white">{params.category}</div>;
+  return (
+    <div className="w-full h-full flex justify-center">
+      <div className="w-[70rem] flex flex-col justify-center items-center py-[4rem]">
+        <div className="flex flex-col gap-[4rem]">
+          <p className="text-white text-5xl font-semibold text-center">
+            Topics in{" "}
+            {params.category.charAt(0).toUpperCase() + params.category.slice(1)}
+          </p>
+
+          <div className="flex flex-wrap justify-center gap-8">
+            {allQuizzes.map((quiz) => {
+              if (
+                quiz.category.toLowerCase() === params.category.toLowerCase()
+              ) {
+                return quiz.quizzes.map((q) => (
+                  <QuizzTitle key={q.id} title={q.title} />
+                ));
+              }
+              return null;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
