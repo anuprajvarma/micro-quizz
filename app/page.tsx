bb38082116525c2d27c7c89332d25df5acf7a0d9
anuprajvarma <anupraj1854@gmail.com>
2025-07-05 03:45:25 +0530

    add: new card programming

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import QuizzCategory from "./component/QuizzCategory";
 export default function Home() {
   return (
     <div className="w-full h-full flex justify-center">
-      <div className="w-[70rem] flex flex-col justify-center items-center py-[6rem]">
-        <div className="flex flex-col gap-12">
+      <div className="w-[70rem] flex flex-col justify-center items-center py-[4rem]">
+        <div className="flex flex-col gap-[6rem]">
           <p className="text-white text-5xl font-semibold text-center">
             Quizz Catagories
           </p>
-          <div className="flex flex-wrap gap-8">
+          <div className="flex flex-wrap justify-center gap-8">
             <QuizzCategory imageLink="/math.avif" category="math" />
             <QuizzCategory imageLink="/science.avif" category="science" />
             <QuizzCategory imageLink="/history.avif" category="history" />
+            <QuizzCategory
+              imageLink="/programming.avif"
+              category="programming"
+            />
           </div>
         </div>
       </div>
