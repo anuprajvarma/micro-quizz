e124fb582df2cb927d82d4f98a850a05c9370bb7
anuprajvarma <anupraj1854@gmail.com>
2025-07-06 12:33:42 +0530

    changes: make qustion button disable if answred

--- app/quizzes/[category]/page.tsx
+++ app/quizzes/[category]/page.tsx
@@ -1,22 +1,24 @@
 "use client";
-import Link from "next/link";
+
 import { allQuizzes } from "@/data/dummuyData";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 // interface Props {
 //   params: Promise<{ category: string }>;
 // }
 
 const Page = () => {
   const params = useParams();
+  const router = useRouter();
   const { category } = params;
   //   const { category } = await props.params;
 
   console.log("Category:", category);
 
+  const questions = useSelector((state: RootState) => state.Questions);
   const score = useSelector((state: RootState) => state.Score);
-
+  console.log("Questions:", questions);
   const selectedQuiz = allQuizzes.find(
     (quiz) => quiz.category.toLowerCase() === category
   );
@@ -37,14 +39,23 @@ const Page = () => {
           <div className="flex flex-col gap-2 w-[40rem]">
             {selectedQuiz ? (
               selectedQuiz.questions.map((question, index) => (
-                <Link
+                <button
+                  disabled={questions.some((q) => q.id === question.id)}
                   key={question.id}
-                  href={`/quiz/${question.id}`}
-                  className="text-gray-900 w-full flex gap-1 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  onClick={() => router.push(`/quiz/${question.id}`)}
+                  //   href={`/quiz/${question.id}`}
+                  className="text-gray-900 w-full flex justify-between bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 >
-                  <p>{index + 1}.</p>
-                  <p>{question.question}</p>
-                </Link>
+                  <div className="flex gap-1">
+                    <p>{index + 1}.</p>
+                    <p>{question.question}</p>
+                  </div>
+                  <p className="text-gray-500">
+                    {questions.some((q) => q.id === question.id)
+                      ? "Answered"
+                      : "Not Answered"}
+                  </p>
+                </button>
               ))
             ) : (
               <p className="text-white text-center">No questions found.</p>
