c8a9135d55f74ea2952e20ced8541cd60e55dc7e
anuprajvarma <anupraj1854@gmail.com>
2025-07-06 12:38:13 +0530

    fix

--- app/quizzes/[category]/page.tsx
+++ app/quizzes/[category]/page.tsx
@@ -44,7 +44,7 @@ const Page = () => {
                   key={question.id}
                   onClick={() => router.push(`/quiz/${question.id}`)}
                   //   href={`/quiz/${question.id}`}
-                  className="text-gray-900 w-full flex justify-between bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  className="text-gray-900 w-full flex justify-between cursor-pointer bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 >
                   <div className="flex gap-1">
                     <p>{index + 1}.</p>
