cbb54be9b900858b15687eaec9222519ebfc1268
anuprajvarma <anupraj1854@gmail.com>
2025-07-06 17:57:02 +0530

    add math image

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -2,11 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const categories = [
-    {
-      imageLink:
-        "https://res.cloudinary.com/anupraj/image/upload/v1656589400/m5f8jkfirkghodqx4i6m.jpg",
-      category: "math",
-    },
+    { imageLink: "/math.jpg", category: "math" },
     { imageLink: "/science.jpg", category: "science" },
     { imageLink: "/history.jpg", category: "history" },
     { imageLink: "/programming.jpg", category: "programming" },
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
